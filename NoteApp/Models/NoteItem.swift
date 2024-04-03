096d7050974a949e1fb664e759ae84a94eaa6caf
fusethnd <fusethnd@gmail.com>
2024-04-03 20:31:57 +0700

    Initial Commit

---
+++ NoteApp/Models/NoteItem.swift
@@ -0,0 +1,8 @@
+//
+//  NoteItem.swift
+//  NoteApp
+//
+//  Created by Thanadon Boontawee on 3/4/2567 BE.
+//
+
+import Foundation
---
+++ NoteApp/Views/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  NoteApp
+//
+//  Created by Thanadon Boontawee on 3/4/2567 BE.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
